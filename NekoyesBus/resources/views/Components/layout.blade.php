f2a6d53c3a23d5fe5760a14ac070b798c8bd42bd
Clement <clement@gmail.com>
2025-06-09 12:19:37 -0400

    v4

--- NekoyesBus/resources/views/Components/layout.blade.php
+++ NekoyesBus/resources/views/Components/layout.blade.php
@@ -4,9 +4,12 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Layout</title>
+        <link rel="stylesheet" href="{{ asset('css/layout.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     </head>
     <body>
         <div class="layoutBody">
+            <x-menu></x-menu>
             {{ $slot }}
         </div>
     </body>
